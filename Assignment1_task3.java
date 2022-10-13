593da189b18b78dad238be52df3484d751b0d292
prasannakulkarni2808 <115685682+prasannakulkarni2808@users.noreply.github.com>
2022-10-12 23:16:53 -0700

    Add files via upload

---
+++ Assignment1_task3.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class Assignment1_task3 {
+
+	//Task 2-  Write a program to print the sum of below 5 numbers- 10,90.78,111,8989,7876//
+	
+	public static void main(String[] args) 
+	{
+		int a=10;
+		double b=90.78;
+		byte c=111;
+		int d=8989;
+		int e=7876;
+		
+		double f=(a+b+c+d+e)/5;
+		
+		System.out.println("The sum of the numbers is "+ f);
+
+	}
+
+}
